706bafd6f2b79f2023d344efc7c0237339f48715
BEERINHO <danieljamesbeer@gmail.com>
2023-03-09 17:29:08 +0100

    Add albums to albums route

--- src/routes/library/albums/+page.svelte
+++ src/routes/library/albums/+page.svelte
@@ -1,1 +1,12 @@
+<script>
+	import { albums } from '@store';
+</script>
+
 <h1>Albums</h1>
+
+<div>
+	{#each $albums as album}
+		<h1>{album.album}</h1>
+		<img src={album.coverUrl} alt={album.album} />
+	{/each}
+</div>
