1babf907901325bfc2cfae5d0a2013e951c5106e
BEERINHO <danieljamesbeer@gmail.com>
2023-03-09 15:04:12 +0100

    Setup aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		alias: {
+			'@components': path.resolve('./src/components'),
+			'@data': path.resolve('./static/data'),
+			'@utils': path.resolve('./src/utils'),
+		},
+	},
 };
 
 export default config;
