1e73a7e50a170d0ce129d5a2ad60dedf7e7fd2b8
BEERINHO <danieljamesbeer@gmail.com>
2023-03-09 17:28:45 +0100

    Add derived stores for albums and artists

--- src/store.js
+++ src/store.js
@@ -1,3 +1,27 @@
-import { writable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 
-export const songs = writable([]);
+export const songs = writable(null);
+
+export const artists = derived(
+  songs,
+  $songs => {
+    return $songs?.reduce((list, { artist }) => {
+      if (!list.includes(artist)) {
+        list.push(artist)
+      }
+      return list
+    }, []) ?? []
+  }
+);
+
+export const albums = derived(
+  songs,
+  $songs => {
+    return $songs?.reduce((list, song) => {
+      if (!list.find(existing => existing.album === song.album)) {
+        list.push(song)
+      }
+      return list
+    }, []) ?? []
+  }
+);
