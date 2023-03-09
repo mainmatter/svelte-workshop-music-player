168946879635a7d08dde64662dfeadf49d19e194
BEERINHO <danieljamesbeer@gmail.com>
2023-03-09 17:30:36 +0100

    Add library nav

---
+++ src/routes/library/library-nav-item.svelte
@@ -0,0 +1,16 @@
+<script>
+	import NowPlaying from './now-playing.svelte';
+	import { page } from '$app/stores';
+	export let href;
+	export let title;
+</script>
+
+<li>
+	<a {href} class:active={$page.url.pathname === href}>{title}</a>
+</li>
+
+<style lang="postcss">
+	.active {
+		@apply text-blue-500;
+	}
+</style>
---
+++ src/routes/library/library-nav.svelte
@@ -0,0 +1,20 @@
+<script>
+	import NowPlaying from './now-playing.svelte';
+	import LibraryNavItem from './library-nav-item.svelte';
+</script>
+
+<nav>
+	<ul class="nav">
+		<LibraryNavItem href="/library/albums" title="Albums" />
+		<LibraryNavItem href="/library/artists" title="Artists" />
+		<LibraryNavItem href="/library/songs" title="Songs" />
+	</ul>
+</nav>
+
+<NowPlaying />
+
+<style lang="postcss">
+	/* .nav a {
+		@apply text-gray-500 hover:text-gray-900;
+	} */
+</style>
