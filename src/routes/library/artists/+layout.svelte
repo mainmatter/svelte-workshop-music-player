5ba45d8030932bd00759ba657dd556a913e56c0f
BEERINHO <danieljamesbeer@gmail.com>
2023-03-09 17:30:05 +0100

    Add artists nav

---
+++ src/routes/library/artists/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import ArtistsNav from './artists-nav.svelte';
+</script>
+
+<h1>Artists</h1>
+<ArtistsNav />
+<slot />
---
+++ src/routes/library/artists/artists-nav.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { artists } from '@store';
+</script>
+
+<div>
+	{#each $artists as artist}
+		<h1>{artist}</h1>
+	{/each}
+</div>
