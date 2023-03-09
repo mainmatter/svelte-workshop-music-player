8e1a6b97fc3c7dfefbf0b266b725428a78d21faf
BEERINHO <danieljamesbeer@gmail.com>
2023-03-09 17:24:23 +0100

    Create primary nav

---
+++ src/components/global/primary-nav.svelte
@@ -0,0 +1,25 @@
+<nav class="sticky top-0 z-50 bg-gray-800 text-gray-200">
+	<div class="flex mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+		<ul class="flex nav-list">
+			<li>
+				<a href="/">Svelte Music</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+			<li>
+				<a href="/library">Library</a>
+			</li>
+		</ul>
+	</div>
+</nav>
+
+<style lang="postcss">
+	.nav-list {
+		@apply flex flex-row list-none gap-5;
+	}
+
+	.nav-list li {
+		@apply m-0 px-0 py-4;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,10 @@
 <script>
+	import PrimaryNav from '@components/global/primary-nav.svelte';
 	import '../app.css';
 </script>
 
-<nav>
-	<ul>
-		<li>
-			<a href="/">Our Music Library</a>
-		</li>
-		<li>
-			<a href="/about">About</a>
-		</li>
-		<li>
-			<a href="/library">Library</a>
-		</li>
-		<li>
-			<a href="/library/albums">Albums</a>
-		</li>
-		<li>
-			<a href="/library/artists">Artists</a>
-		</li>
-		<li>
-			<a href="/library/songs">Songs</a>
-		</li>
-	</ul>
-</nav>
+<PrimaryNav />
 
 <hr />
 
 <slot />
-
-<style>
-	nav {
-		height: 50px;
-		display: flex;
-		align-items: center;
-	}
-	ul {
-		display: flex;
-		gap: 15px;
-		width: 80vw;
-		margin: auto;
-	}
-</style>
