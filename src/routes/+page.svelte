39f9a420531d19b1a82b3d2138cd2cd8094fc3dc
BEERINHO <danieljamesbeer@gmail.com>
2023-06-02 10:02:15 +0200

    Lection 1: Add static HTML

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to The Svelte Music Player</h1>
+
 <p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+	Billy Bloggs, it's great to see you again!
 </p>
