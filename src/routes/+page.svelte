39f6e59db660efcc3ae99feb7904cb23fdebda9a
BEERINHO <danieljamesbeer@gmail.com>
2023-06-02 10:02:15 +0200

    Sveltekit Basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const name = 'Billy Bloggs';
+</script>
+
+<h1>Welcome to The Svelte Music Player</h1>
+
+<p>
+	{name}, it's great to see you again!
+</p>
