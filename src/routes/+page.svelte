2e6be94c385c61ac2005f147a7758eb0a862fba0
Florian Pichler <fp@ylk.gd>
2023-09-01 17:40:05 +0200

    Lection 2: Preparing Svelte Components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,27 @@
 	let name = 'Billy Bloggs';
 </script>
 
-<h1>Welcome to The Svelte Music Player</h1>
+<!-- Put PrimaryNav here -->
 
-<p>
-	{name}, it's great to see you again!
-</p>
+<section>
+	<p class="title">Cybernetically enhanced playback</p>
+	<p>Just for you, {name}</p>
+
+	<!-- Put Boxes here -->
+</section>
+
+<style lang="postcss">
+	section {
+		@apply bg-zinc-200 flex flex-col justify-end;
+		min-height: 50vh;
+		margin-bottom: 8vw;
+	}
+
+	.title {
+		@apply container mx-auto py-8 text-center text-5xl font-bold text-zinc-600;
+	}
+
+	p {
+		@apply container mx-auto text-center text-xl font-bold text-zinc-600;
+	}
+</style>
---
+++ src/routes/boxes.svelte
@@ -0,0 +1,45 @@
+<script>
+	export let colors = ['red', 'green', 'blue', 'yellow', 'purple'];
+
+	let colorIndices = [0, 1, 2];
+
+	$: color1 = colors[colorIndices[0]];
+	$: color2 = colors[colorIndices[1]];
+	$: color3 = colors[colorIndices[2]];
+
+	function shuffleColor(index) {
+		const colorArray = [...colorIndices];
+
+		do {
+			const randomIndex = Math.floor(Math.random() * colors.length);
+			colorArray[index] = randomIndex;
+		} while (colorArray[index] === colorIndices[index]);
+
+		colorIndices = colorArray;
+	}
+</script>
+
+<div class="box-container">
+	<button style="background:{color1}" type="button">
+		<!--  -->
+	</button>
+	<button style:background={color2} type="button">
+		<!--  -->
+	</button>
+	<button style:background={color3} type="button">
+		<!--  -->
+	</button>
+</div>
+
+<style lang="postcss">
+	.box-container {
+		@apply flex relative container mx-auto;
+		top: 8vw;
+		margin: 0 auto;
+		gap: 2rem;
+	}
+
+	button {
+		@apply block w-full aspect-video text-white px-5 py-4;
+	}
+</style>
