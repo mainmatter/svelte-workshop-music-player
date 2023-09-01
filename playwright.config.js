d36e2fe705dfd1e76b07704a9a15fb8f4f73f4d0
Florian Pichler <fp@ylk.gd>
2023-09-01 16:53:41 +0200

    Lection 1: Svelte Basics
    - Add a new headline "Welcome to The Svelte Music Player"
    - Add a new paragraph "Billy Bloggs, it's great to see you again!"

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,19 @@
+let webServer = {
+	// Static test environment is recommended for CI:
+	command: 'npm run build && npm run preview',
+	port: 4173,
+};
+
+if (process.argv.includes('--ui')) {
+	webServer = {
+		command: 'npm run dev',
+		port: 5173,
+	};
+}
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173,
-	},
+	webServer,
 	testDir: 'tests',
 };
 
--- tests/test.js
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('visiting / renders the index page', async ({ page }) => {
-	await page.goto('/');
-
-	await expect(page).toHaveURL('/');
-	await expect(page.locator('[data-testId="select-album-btn"]')).toHaveCount(2);
-	await expect(page.locator('[data-testId="song"]')).toHaveCount(20);
-});
